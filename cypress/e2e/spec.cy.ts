ee20c17ff710087c03ee411874c078486eab8f27
David Jensen <tsourdox@icloud.com>
2025-08-26 10:37:16 +0200

    setup cypress with first test spec

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,8 @@
+describe("todo", () => {
+  it("should display three todos by default", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("li").should("have.length", 3);
+    cy.get("li").first().should("contain.text", "Feed the cat");
+    cy.get("li").last().contains("Walk all the cats");
+  });
+});
